ba283ee8b32d023fbfb4afd25bbb605341ebf90a
Jonathan <jono_95@yahoo.com>
2023-03-25 21:32:45 -0500

    commit tree 5b6deed8b4c757e336dfe7376c6ca61c7b484a2e

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,34 @@
+class RecipesController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def index
+        user = User.find_by(id: session[:user_id])
+        if user
+            recipes = Recipe.all
+            render json: recipes, status: :created
+        else
+            render json: { errors: ["Not authorized"] }, status: :unauthorized
+        end
+    end
+
+    def create
+        user = User.find_by(id: session[:user_id])
+        if user
+            recipe = user.recipes.create!(recipe_params)
+            render json: recipe, status: :created
+        else
+            render  json: { errors: ["Not authorized"] }, status: :unauthorized
+        end
+    end
+
+    private
+
+    def recipe_params
+        params.permit(:title, :instructions, :minutes_to_complete)
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: {errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+end
